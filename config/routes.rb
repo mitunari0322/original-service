a43b8894169cc0edceb63fe5f76428ae0dc77e4a
mitsunari <mitsunari0322@gmail.com>
2020-10-24 06:26:18 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :new, :create]
+  root to: "users#index"
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
+  resources :users, only: [:show, :new, :create]
 end
